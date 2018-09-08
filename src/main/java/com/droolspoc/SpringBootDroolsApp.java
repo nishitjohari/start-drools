10478c69b1568c2d0f6da93e8eee454d73e54d68
u1161482 <u1161482@USPHXMLFTVQPQ2.ad.us.mrshmc.com>
2018-09-08 13:23:20 -0700

    war pack

--- src/main/java/com/droolspoc/SpringBootDroolsApp.java
+++ src/main/java/com/droolspoc/SpringBootDroolsApp.java
@@ -4,17 +4,24 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class SpringBootDroolsApp {
+public class SpringBootDroolsApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDroolsApp.class, args);
 	}
 	
-	@Bean
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootDroolsApp.class);
+    }
+	
+	@Bean 
 	public KieContainer kieContainer() {
 		return KieServices.Factory.get().getKieClasspathContainer();
 	} 
